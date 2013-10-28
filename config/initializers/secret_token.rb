598fc507fe361756a2da05563ca13bcab8e686f3
carl sednaoui <carlsednaoui@gmail.com>
2013-10-28 01:22:36 -0400

    Adds secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootcampsearch::Application.config.secret_key_base = '41bd7faac659666d74a6a5c97e1801c46eba4bac89ed49e048e262bfdec8a36bf0f82ec8ea03da7793b1555e73e3e54a763811b39be427092f373eba1a79a30d'
