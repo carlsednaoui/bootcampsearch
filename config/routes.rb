75af0db25316a8d46bd330872a611c66e566215b
carl sednaoui <carlsednaoui@gmail.com>
2013-10-28 22:38:20 -0400

    Adds homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bootcampsearch::Application.routes.draw do
+  root "static#home"
+  
   # enables use of /coding-bootcamp instead of /coding_bootcamp
   resources "coding-bootcamp", :controller => :coding_bootcamps, :as => :coding_bootcamps
 end
