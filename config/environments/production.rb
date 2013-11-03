c8118c21e802521aea59acef0836400eb6b4c47b
carl sednaoui <carlsednaoui@gmail.com>
2013-11-03 17:35:26 -0500

    Adds icon fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,13 @@ Bootcampsearch::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Used to precompile iconmoon fonts
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
