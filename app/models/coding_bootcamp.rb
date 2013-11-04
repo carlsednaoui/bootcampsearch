b3c13e277d5e06770ab2d76b885ce4210cba90ee
carl sednaoui <carlsednaoui@gmail.com>
2013-11-03 19:52:44 -0500

    Fix to create friendly id slug

--- app/models/coding_bootcamp.rb
+++ app/models/coding_bootcamp.rb
@@ -4,7 +4,7 @@ class CodingBootcamp < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
 
-  def should_generate_new_friendly_id?
-    name_changed?
-  end
+  # def should_generate_new_friendly_id?
+  #   name_changed?
+  # end
 end
