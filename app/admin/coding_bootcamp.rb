fb248457e17929dfc5cf1b7e6fb3957e19c72c5e
carl sednaoui <carlsednaoui@gmail.com>
2013-11-03 21:52:52 -0500

    Fixes active admin permisions

--- app/admin/coding_bootcamp.rb
+++ app/admin/coding_bootcamp.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register CodingBootcamp do
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+
+    def permitted_params
+      params.permit(:coding_bootcamp => [:name, :website, :description])
+    end
+  end
+
   form do |f|
      f.inputs 'Details' do
       f.input :name
