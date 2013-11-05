4c2d5db68b6cf524bbe33a60c661cd84e848b588
carl sednaoui <carlsednaoui@gmail.com>
2013-11-05 00:17:05 -0500

    Adds compiled assets for Heroku

---
+++ public/assets/active_admin/components/application-0bde36f4647fe6936f1b87588da6430e.js
@@ -0,0 +1,27 @@
+(function() {
+  $(function() {
+    $(document).on('focus', '.datepicker:not(.hasDatepicker)', function() {
+      var defaults, options;
+      defaults = {
+        dateFormat: 'yy-mm-dd'
+      };
+      options = $(this).data('datepicker-options');
+      return $(this).datepicker($.extend(defaults, options));
+    });
+    $('.clear_filters_btn').click(function() {
+      return window.location.search = '';
+    });
+    $('.dropdown_button').popover();
+    $('.filter_form').submit(function() {
+      return $(this).find(':input').filter(function() {
+        return this.value === '';
+      }).prop('disabled', true);
+    });
+    return $('.filter_form_field.select_and_search select').change(function() {
+      return $(this).siblings('input').prop({
+        name: "q[" + this.value + "]"
+      });
+    });
+  });
+
+}).call(this);
