7757a75782cbd2f94c30eca0a7e1977905242643
carl sednaoui <carlsednaoui@gmail.com>
2013-11-29 18:06:05 -0500

    Adds locations to header location dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :getBootcampProgramsCities
+
+  def getBootcampProgramsCities
+    @cities = Program.all.map(&:location_list).flatten.uniq
+  end
 end
