ece76939f9e6a843df6035c6358d21b082fde7fa
carl sednaoui <carlsednaoui@gmail.com>
2013-11-30 22:12:07 -0500

    Fixes bootcamp show layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :getBootcampProgramsCities
+  before_filter :getBootcampProgramsLanguages
 
   def getBootcampProgramsCities
     @cities = Program.all.map(&:location_list).flatten.uniq
+  end
+
+  def getBootcampProgramsLanguages
+    @languages = Program.all.map(&:language_list).flatten.uniq
   end
 end
