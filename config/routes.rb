b14594afade1ca3201d1d0359ea499ab042c9125
carl sednaoui <carlsednaoui@gmail.com>
2013-12-01 00:55:41 -0500

    Adds about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Bootcampsearch::Application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  # root "static#home"
   root "static#home"
+  get "/about", to: "static#about"
   resources :programs
 
-  get 'location/:tag', to: 'coding_bootcamps#index', as: :location
-  get 'language/:tag', to: 'coding_bootcamps#index', as: :language
+  get "location/:tag", to: "coding_bootcamps#index", as: :location
+  get "language/:tag", to: "coding_bootcamps#index", as: :language
 
   # enables use of /coding-bootcamp instead of /coding_bootcamp
   resources "programming-bootcamps", :controller => :coding_bootcamps, :as => :coding_bootcamps
